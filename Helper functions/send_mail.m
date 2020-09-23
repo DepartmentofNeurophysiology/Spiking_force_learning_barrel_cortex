6f920fada9963b4754f7fabbb412718467d5395a
HendrikScheeres <you@example.com>
2020-09-23 14:43:10 +0200

    updated send mail function

--- Helper functions/send_mail.m
+++ Helper functions/send_mail.m
@@ -2,15 +2,15 @@ function  send_mail( text )
 %UNTITLED2 Summary of this function goes here
 %   Detailed explanation goes here
 
-setpref('Internet','SMTP_Server','smtp.gmail.com');
-setpref('Internet','E_mail','timwinter06@gmail.com');
-setpref('Internet','SMTP_Username','timwinter06');
-setpref('Internet','SMTP_Password','timber1995');
+setpref('Internet','SMTP_Server','smtp-mail.outlook.com');
+setpref('Internet','E_mail','hscheeres@hotmail.com');
+setpref('Internet','SMTP_Username','hscheeres@hotmail.com');
+setpref('Internet','SMTP_Password','');
 props = java.lang.System.getProperties;
 props.setProperty('mail.smtp.auth','true');
-props.setProperty('mail.smtp.socketFactory.class', 'javax.net.ssl.SSLSocketFactory');
-props.setProperty('mail.smtp.socketFactory.port','465');
-sendmail('timwinter06@gmail.com',text) ;
+props.setProperty('mail.smtp.starttls.enable', 'true');
+props.setProperty('mail.smtp.socketFactory.port','587');
+sendmail('hscheeresyt@gmail.com',text) ;
 
 end
 
