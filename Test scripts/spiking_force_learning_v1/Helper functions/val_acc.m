21b8e093b5cf773a6bdc8ef04249799dbcbde8ea
HendrikScheeres <you@example.com>
2020-10-01 15:10:24 +0200

    adjustments for trial spike maker

---
+++ Test scripts/spiking_force_learning_v1/Helper functions/val_acc.m
@@ -0,0 +1,46 @@
+function [ acc ] = val_acc(epoch, network_output, target, first_touches )
+% Calculate the validation accuracy 
+num_val = epoch; % number of epochs
+mean_network = zeros(num_val, 1);
+mean_target = zeros(num_val, 1);
+
+
+%% Calculate the mean value each trial
+for i = 1:num_val
+    z_t = network_output{i};
+    zx = target{i};
+      
+    % trial_length = z_t - 1200;
+    % Only calculate mean when output is trained
+    %s_t = length(zx) - 800 - 500;
+    s_t = first_touches(i);
+    if s_t == 0
+        s_t = 1000;
+    elseif s_t < 0
+        s_t = 1000;
+    end
+    %s_t = 1000;
+    mean_target(i) =  mean(zx(s_t:end));
+    mean_network(i) = mean(z_t(s_t:end));
+end
+%% Set a classifcation boundary, mean positive value or mean negative value.
+network_answer = zeros(num_val,1);
+correct_answer = zeros(num_val, 1);
+for i = 1:num_val
+    if mean_network(i) > 0
+        network_answer(i) = 1;
+    else
+        network_answer(i) = -1;
+    end
+    if mean_target(i) > 0
+        correct_answer(i) = 1;
+    else
+        correct_answer(i) = -1;
+    end
+end
+%% Calculate the accuracy 
+multi = network_answer .* correct_answer; % All positive values indicated correctly classified val_trials
+correct = sum(multi==1); % Count total positive values
+acc = correct./num_val;
+end
+
