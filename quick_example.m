357c4eb16d7ccc5acfb32e70656bf511ee25f2aa
Loisdegroot <loisdegroot44@gmail.com>
2021-06-21 11:20:42 +0200

    Added descriptions

--- quick_example.m
+++ quick_example.m
@@ -1,31 +1,34 @@
 %% Quick example spiking force learning barrel cortex
-
-%% savefolder for output files
+% Runs short simulation of the network
+%% Savefolder for output files
 f = filesep;
 savename = 'quick_example';
 savefolder = ['.' f 'Output' f savename];
 %% Set the parameters to run the simulation
+% input parameters
 N = 2000;        % number of neurons
-N_th = 200;     % number of thalamus neurons
-N_train = 2;    % number of training trials
-N_test = 2;     % number of validation trials
-N_total = 1;    % number of epochs
+N_th = 200;      % number of thalamus neurons
+N_train = 2;     % number of training trials
+N_test = 2;      % number of validation trials
+N_total = 1;     % number of epochs
 
-% varying parameters
-Win = [0.5];       % scales the input weights
-G = [10];         % scales the static weights
-Q = [0];         % scales the feedback weights
-Winp = [1];      % network sparsity
+% weight scaling parameters
+Win = 0.5;       % scales the input weights
+G = 10;          % scales the static weights
+Q = 1;           % scales the feedback weights
+Winp = 1;        % network sparsity
 alpha = 0.05;    % learning rate
 
 % logicals
-FORCE = true;   % apply FORCE learning during trials
-makespikes = false; % make the trial spiking structures 
-input_type = 'ConvTrace'; % options: 'ConvTrace', 'PSTH', 'spikes'
+FORCE = true;       % if TRUE; apply FORCE learning during trials
+makespikes = true;  % if TRUE; make the trial spiking structures 
 
 % percentage of excitatory neurons if Dale's law is applied
-Pexc = 0;     % set to 0 to ignore Dale's law restrictions
-%% run the simulation
+Pexc = 0;   % percentage of excitatory neurons; set to 0 to ignore Dale's law restrictions
+
+% Input 
+input_type = 'spikes';  % options: 'ConvTrace', 'PSTH', 'spikes'
+%% Run the simulation
 run = run_sim(N, N_th, N_train, N_test, N_total, Win, G, Q, Winp,...
     alpha, Pexc, FORCE, makespikes, input_type, savefolder);
 
